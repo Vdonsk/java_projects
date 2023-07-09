44914d7de241e4548b1160de86740757fafb06bb
Андрей <vdon61@mail.ru>
2023-07-09 15:24:27 +0300

    java

---
+++ program.java
@@ -0,0 +1,88 @@
+/**
+ * program
+ */
+// public class program {
+//     public static void main(String[] args) {
+//         System.out.println("World");
+//     }
+
+// }
+
+/**
+ * program
+ */
+// public class program {
+//     public static void main(String[] args) {
+//         // float f = 123.45f;
+//         // System.out.println(f);
+
+//         // double d = 543.12;
+//         // System.out.println(d);
+
+//         // boolean b = true ^ false;
+//         // System.out.println(b);
+
+//         // String msg = "Hello World";
+//         // String msg2 = "!!!";
+//         // System.out.println(msg + " " +msg2);
+
+//         var i = "123";
+//         System.out.println(i);
+//         System.out.println(getType(i));
+//     }
+//         static String getType(Object o) {
+//             return o.getClass().getSimpleName();
+//     }
+// }    
+
+// /**
+//  * program
+//  */
+// public class program {
+//     public static void main(String[] args) {
+
+//     // int i = 123_45_678;
+//     // System.out.println(i);
+//     // System.out.println(Integer.MAX_VALUE);
+//     // System.out.println(Integer.MIN_VALUE);
+
+//     // String s = "qwer";
+//     // System.out.println(s.charAt(1));
+//     }
+// }
+
+/**
+ * program
+ */
+
+// public class program {
+//     public static void main(String[] args) {
+
+//         // int a = 123;
+//         // a++;
+//         // System.out.println(a++);
+//         // System.out.println(++a);
+//         // System.out.println(a);
+
+//         // boolean f = 123 > 456;
+//         // boolean f = 123 < 456;
+//         // boolean f = 123 != 456;
+//         // boolean f = 123 == 456;
+//         // System.out.println(f);
+
+//         // int a = 8; // 1000
+//         // // a = a << 1; // 10000
+//         // System.out.println(a >> 2);
+
+//         // int a = 5;
+//         // int b = 2;
+//         // System.out.println(a | b); // 101 010 111
+//         // System.out.println(a & b); // 101 010 000
+//         // System.out.println(a ^ b); // 101 010 000
+
+//         boolean a = true;
+//         boolean b = true;
+//         System.out.println(a & b);
+//         System.out.println(a && b);
+//     }
+// }
